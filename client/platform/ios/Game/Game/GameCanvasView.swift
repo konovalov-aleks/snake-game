b565ce12e6b8006bca6631314ec851d94acce10f
Солодовников П.А <hellyeahdominate@gmail.com>
2016-11-18 12:13:11 +0300

    Bonus rendering fix

--- client/platform/ios/Game/Game/GameCanvasView.swift
+++ client/platform/ios/Game/Game/GameCanvasView.swift
@@ -121,7 +121,6 @@ class GameCanvasView: UIView {
         var bonusRegionSize: CGFloat = mmToPoints(2)
         
         ctx.saveGState()
-        ctx.setShadow(offset: CGSize(width: 0, height: 0), blur: 2.5, color: blackCGColor)
         for i in 1...3 {
             ctx.saveGState()
             // эмулируем LightingColorFilter из android
@@ -134,7 +133,7 @@ class GameCanvasView: UIView {
             else if cgColorComponents.count == 2 {
                 adjustedCol = UIColor(white: cgColorComponents[0] * CGFloat(128 + i * 40) / 255, alpha: 1.0)
             }
-            bonusRegionSize = CGFloat(2.0 - 0.4 * Double(i))
+            bonusRegionSize = mmToPoints(2.0) - mmToPoints(CGFloat(0.2 * Double(i)))
             ctx.setFillColor(adjustedCol.cgColor)
             ctx.fillEllipse(in: CGRect(x: mmToPoints(CGFloat(bonus.x)) - bonusRegionSize / 2,
                                        y: mmToPoints(CGFloat(bonus.y)) - bonusRegionSize / 2, width: bonusRegionSize, height: bonusRegionSize))
